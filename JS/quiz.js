8b82886aab15dd9c0409d48306f5ba073275a8cb
Manav Doda <manavdoda7@gmail.com>
2021-03-26 01:25:26 +0530

    Quiz page added

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,114 +1,75 @@
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const submitButton = document.getElementById("submit");
 
+let currentQuiz = 0;
+let score = 0;
 
-const quizData = [
-    {
-      question: "Your sleep hour duration is around 7-8 hours on most days",
-      a: "Yes",
-      b: "No",
-      correct: "a",
-    },
-    {
-      question: "Poor appetite or overeating",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-      question: "Excessive junk food or drug/alcohol consumption",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Poor interaction with family, friends and co-mates",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Ever been through a traumatic experience in past due to which you have any unwanted and recurring memories, flashbacks or vivid nightmares",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Under any kind of parental, peer or performance pressure",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Guilt about any decision about your personal or personal life taken by you or by your parents",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Problems accepting your sexuality by your own-self or by society",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Inferiority complex about anything",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-    {
-        question: "Felt sad or lonely or had suicidal thoughts",
-      a: "Yes",
-      b: "No",
-      correct: "b",
-    },
-  ];
-  
-  const quiz = document.getElementById("quiz");
-  const answerElements = document.querySelectorAll(".answer");
-  const questionElement = document.getElementById("question");
-  const a_text = document.getElementById("a_text");
-  const b_text = document.getElementById("b_text");
-  const submitButton = document.getElementById("submit");
-  
-  let currentQuiz = 0;
-  let score = 0;
-  
-  const deselectAnswers = () => {
+const deselectAnswers = () => {
     answerElements.forEach((answer) => (answer.checked = false));
-  };
-  
-  const getSelected = () => {
+};
+
+const getSelected = () => {
     let answer;
     answerElements.forEach((answerElement) => {
-      if (answerElement.checked) answer = answerElement.id;
+        if (answerElement.checked) answer = answerElement.id;
     });
     return answer;
-  };
-  
-  const loadQuiz = () => {
+};
+
+const loadQuiz = () => {
     deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
     questionElement.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
-  };
-  
-  loadQuiz();
-  
-  submitButton.addEventListener("click", () => {
+};
+
+loadQuiz();
+
+submitButton.addEventListener("click", () => {
     const answer = getSelected();
     if (answer) {
-      if (answer === quizData[currentQuiz].correct) score++;
-      currentQuiz++;
-      if (currentQuiz < quizData.length) loadQuiz();
-      else if{}
-      else {
-        quiz.innerHTML = `
+        if (answer === quizData[currentQuiz].correct) score++;
+        currentQuiz++;
+        if (currentQuiz < quizData.length) loadQuiz();
+        else if (score <= (quizData.length) / 3) {
+
+            quiz.innerHTML = `
               <h2>Your score is ${score}/${quizData.length}.</h2>
-              
+              <h6 style= "color: red; padding-left: 8px;">Your condition is severly bad. Consult a psychiatrist asap.<br>Below are some website recommendations:</h6>
+              <div style="margin-left:8px;"><a href="https://thervo.com/psychiatrists"Thervo</a><br>
+              <a href="https://doctor.webmd.com/find-a-doctor/specialty/psychiatry">WebMD</a><br>
+              <a href="https://www.lybrate.com/psychiatrist">Lybrate</a><br></div>
               <button onclick="history.go(0)">Take the quiz again.</button>
-          ` // location.reload() won't work in CodePen for security reasons;
-      }
+          `
+        }
+        else if (score <= 2 * (quizData.length) / 3) {
+
+            quiz.innerHTML = `
+              <h2>Your score is ${score}/${quizData.length}.</h2>
+              <h6 style= "color: orange; padding-left: 8px;">You ain't that good to go. But you can be cured. Calm yourself down and give some time to figure out your thoughts .<br>Stay positive! Below are some links to youtube playlists: </h6>
+              <div style="margin-left:8px;"><a href="https://youtu.be/Tm8LGxTLtQk">One more light</a><br>
+              <a href="https://youtu.be/1srs1YoTVzs">Fear is a liar</a><br>
+              <a href="https://youtu.be/j2WWrupMBAE">Who you are</a><br>
+              <a href="https://youtu.be/q7yCLn-O-Y0">Carry On</a><br>
+              <a href="https://youtu.be/IYzlVDlE72w">The Greatest love of all</a><br>
+              <a href="https://youtu.be/fgmpWkUcpjo">Not gonna die</a><br>
+              <a href=" https://youtu.be/xo1VInw-SKc">Fight Song</a><br>
+              <a href="https://youtu.be/CjxugyZCfuw">This is me</a><br>
+              <a href="https://youtu.be/RcohgARJTWQ">Get Up</a><br></div>
+              <button onclick="history.go(0)">Take the quiz again.</button>
+          `
+        }
+        else {
+            quiz.innerHTML = `
+              <h2>Your score is ${score}/${quizData.length}.</h2>
+              <h6 style= "color: green; padding-left: 8px;">You're good to go. Just hang out a little with your buddies and you'll feel relaxed.</h6>
+              <button onclick="history.go(0)">Take the quiz again.</button>
+          `
+        }
     }
-  });
-  
+});
---
+++ JS/quiza.js
@@ -0,0 +1,74 @@
+const quizData = [
+    {
+      question: "Your sleep hour duration is around 6-7 hours on most days",
+      a: "Yes",
+      b: "No",
+      correct: "a",
+    },
+    {
+      question: "Poor appetite or overeating",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+      question: "Excessive drug/alcohol consumption",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Poor interaction with family, friends and co-mates",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Ever been through a traumatic experience in past due to which you have any unwanted and recurring memories, flashbacks or vivid nightmares",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Are your working hours above 10 hours",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Are you financially independent",
+      a: "Yes",
+      b: "No",
+      correct: "a",
+    },
+    {
+        question: "Are you living a happy marital life",
+      a: "Yes",
+      b: "No",
+      correct: "a",
+    },
+    {
+        question: "Burdened by home and work responsibilities most of the time",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Have time for relaxing mind by doing yoga or meditation or any other recreational activity ",
+      a: "Yes",
+      b: "No",
+      correct: "a",
+    },
+    {
+        question: "Problems accepting your sexuality by your own-self or by society",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+    {
+        question: "Felt sad or lonely or had self-desctructive thoughts",
+      a: "Yes",
+      b: "No",
+      correct: "b",
+    },
+  ];
